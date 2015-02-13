63394e133d3524d07c6725b9dfbc8d14156a0ac1
abudnik <budnik27@gmail.com>
2015-02-13 20:41:00 +0300

    prime numbers in range finder

---
+++ primes.hs
@@ -0,0 +1,30 @@
+
+import Data.Char as Char
+
+splitStr :: (Char -> Bool) -> String -> [String]
+splitStr f "" = []
+splitStr f s = let (word, part) = break f (dropWhile f s)
+               in word : splitStr f (dropWhile f part)
+
+checkDivisors :: Int -> Int -> Int -> Bool
+checkDivisors v d maxd = if d <= maxd then
+                             if mod v d == 0 then False
+                             else checkDivisors v (d + 1) maxd
+                         else True
+
+isPrime :: Int -> Bool
+isPrime v = if v > 2 then
+                if odd v then checkDivisors v 3 ((floor . sqrt . fromIntegral) v) else False
+            else False
+
+findPrimes :: Int -> Int -> [Int]
+findPrimes fst lst = filter isPrime [fst..lst]
+
+main :: IO ()
+main = do
+  contents <- readFile "input.txt"
+  let words = splitStr Char.isSpace contents
+  let first = read (head words) :: Int
+  let last = read ((head . tail) words) :: Int
+  mapM_ (putStr . (++ ",") . show) (findPrimes first last)
+
